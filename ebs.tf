13d86ed144378e6b07fa9c708a44a4970e9df74b
Barak Schoster Goihman <schosterbarak@gmail.com>
2020-05-31 21:49:30 +0300

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -10,11 +10,6 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
-provider "aws" {
-  version = "2.33.0"
-
-  region = var.aws_region
-}
 
 provider "random" {
   version = "2.2"
@@ -25,16 +20,12 @@ resource "random_pet" "table_name" {}
 resource "aws_dynamodb_table" "tfc_example_table" {
   name = "${var.db_table_name}-${random_pet.table_name.id}"
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
+  read_capacity  = 5
+  write_capacity = 5
   hash_key       = "UUID"
 
   attribute {
     name = "UUID"
     type = "S"
-  }
-
-  tags = {
-    user_name = var.tag_user_name
   }
 }
